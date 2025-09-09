8a0a29037cb27586260a8c51cb539b94ca70e329
Yayiyi94 <yadiratr94@gmail.com>
2025-09-09 14:27:47 -0600

    added  Dictionaty API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Searching for ${keyword}...`);
+
+    let apiKey = "10fa90a2o832483bf734tfe8a27fcdad";
+    // API link documentation https://www.shecodes.io/learn/apis/dictionary
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeyword(event) {
