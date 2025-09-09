78f0f7b43628894fa666261c2f6bf3857416ab16
Yayiyi94 <yadiratr94@gmail.com>
2025-09-09 13:28:56 -0600

    Changes on main CSS, added dictionary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import logo from "./dictionary-logo.png";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-        <p>Hello there</p>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1>Hello there</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer></footer>
+      </div>
     </div>
   );
 }
